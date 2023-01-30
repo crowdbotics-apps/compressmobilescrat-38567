06435af0e1dd609fcb93aa74851db409908f10d7
Crowdbotics <team@crowdbotics.com>
2023-01-30 02:00:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "compressmobilescrat-38567",
+  url: "https://compressmobilescrat-38567.botics.co",
+  api: "https://compressmobilescrat-38567.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
